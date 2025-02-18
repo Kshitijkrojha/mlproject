17db5652e212593c08e1c04dbede738c32e75fa8
Kshitijkrojha <arhivamth11@gmail.com>
2025-02-19 01:54:14 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -45,8 +46,15 @@ class DataIngestion:
             raise customexception(e,sys)
 
 if __name__=='__main__':
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    try:
+        obj = DataIngestion()
+        train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+        data_transformation = DataTransformation()
+        train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+    except Exception as e:
+        logging.error("Error in the main execution")
+        raise customexception(e, sys)
